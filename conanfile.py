bd999d11bfddefd70ef31eacffad6ddf9a20a364
Matthew Russell <mrussell@neptec.com>
2018-02-27 11:53:30 -0500

    Modified URL

--- conanfile.py
+++ conanfile.py
@@ -1,36 +1,35 @@
-from __future__ import print_function
-from conans import ConanFile, CMake, tools
-from glob import glob
-from time import sleep
-
 import os
-import subprocess
+from conans import ConanFile, tools, AutoToolsBuildEnvironment
+from glob import glob
+
 
 class PkgConfigConan(ConanFile):
     name = 'pkg-config'
-    version = '0.29.1'
+    version = '0.29.2'
     license = 'MIT'
-    url = 'https://github.com/sztomi/pkg-config-conan'
+    url = 'https://github.com/kheaactua/pkg-config-conan'
     description = 'This is a tooling package for pkg-config'
     settings = 'os', 'compiler', 'build_type', 'arch'
-    generators = 'cmake', 'virtualenv'
 
     def source(self):
-        tarball_url = 'https://pkgconfig.freedesktop.org/releases/pkg-config-{}.tar.gz'.format(self.version)
+        tarball_url = f'https://pkgconfig.freedesktop.org/releases/pkg-config-{self.version}.tar.gz'
         tgz = tarball_url.split('/')[-1]
         tools.download(tarball_url, tgz)
         tools.untargz(tgz)
         os.unlink(tgz)
 
     def build(self):
-        self.dirname = glob('pkg-config-*')[0]
-        os.chdir(self.dirname)        
-        def run_in_env(cmd):
-            activate = '. ../activate.sh &&'
-            self.run(activate + cmd)
-        run_in_env('./configure --with-internal-glib --prefix={}'.format(self.package_folder))
-        self.run('make')
-        self.run('make install')
+        dirname = glob('pkg-config-*')[0]
+
+        autotools = AutoToolsBuildEnvironment(self, win_bash=('Windows' == self.settings.os))
+
+        with tools.chdir(dirname):
+            args = []
+            args.append('--with-internal-glib')
+            args.append(f'--prefix={self.package_folder}')
+            autotools.configure(args=args)
+            autotools.make()
+            autotools.make(args=['install'])
 
     def package(self):
         pass
@@ -38,4 +37,5 @@ class PkgConfigConan(ConanFile):
     def package_info(self):
         self.env_info.path.append(os.path.join(self.package_folder, 'bin'))
         self.env_info.path.append(os.path.join(self.package_folder, 'share/aclocal'))
-        
+
+# vim: ts=4 sw=4 expandtab ffs=unix ft=python foldmethod=marker :
